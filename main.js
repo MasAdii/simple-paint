78504f36b0aa6200d053f148491bd5fd0dcb272c
masadii <githubmasadi@gmail.com>
2025-04-20 07:33:22 +0700

    finish

---
+++ main.js
@@ -0,0 +1,71 @@
+const canvas = document.getElementById('canvas')
+const ctx = canvas.getContext('2d')
+
+const brush = document.getElementById('brush')
+const eraser = document.getElementById('eraser')
+const clear = document.getElementById('clear')
+const colorPicker = document.getElementById('colorPicker')
+
+canvas.width = 800
+canvas.height = 500
+
+let painting = false
+let erasing = false
+let currentColor = '#000000'
+let lineWidth = 5
+
+function start(e){
+    painting = true
+    draw(e)
+}
+
+function end(e){
+    painting = false
+    ctx.beginPath()
+}
+
+function draw(e){
+    if(!painting) return
+
+    ctx.lineWidth = lineWidth
+    ctx.lineCap = 'round'
+    ctx.strokeStyle = erasing ? '#FFFFFF' : currentColor
+
+    const rect = canvas.getBoundingClientRect();
+    const x = e.clientX - rect.left;
+    const y = e.clientY - rect.top;
+
+    ctx.lineTo(x, y);
+    ctx.stroke();
+    ctx.beginPath();
+    ctx.moveTo(x, y);
+}
+
+function selectBrush(){
+    erasing = false
+    brush.classList.add('active')
+    eraser.classList.remove('active')
+}
+
+function selectEraser(){
+    erasing = true
+    eraser.classList.add('active')
+    brush.classList.remove('active')
+}
+
+function clearCanvas(){
+    ctx.clearRect(0, 0, canvas.width, canvas.height)
+}
+
+function changeColor(e){
+    currentColor = e.target.value
+}
+
+brush.addEventListener('click', selectBrush)
+eraser.addEventListener('click', selectEraser)
+clear.addEventListener('click', clearCanvas)
+colorPicker.addEventListener('input', changeColor)
+
+canvas.addEventListener('mousedown', start)
+canvas.addEventListener('mouseup', end)
+canvas.addEventListener('mousemove', draw)
